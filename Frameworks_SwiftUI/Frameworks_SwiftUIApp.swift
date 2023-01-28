bd037e1b75f1fa5043738fc40d0a4fd03fff2277
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2023-01-28 19:46:12 +0200

    Initial Commit

---
+++ Frameworks_SwiftUI/Frameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frameworks_SwiftUIApp.swift
+//  Frameworks_SwiftUI
+//
+//  Created by AnatoliiOstapenko on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Frameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
